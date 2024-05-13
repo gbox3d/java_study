b98fa418d2808e3ecdc08eb1d50d80d2f1348477
gbox3d <gbox3d@gmail.com>
2024-05-14 01:53:59 +0900

    ...

---
+++ chapter6_package/src/App.java
@@ -0,0 +1,15 @@
+import mypack.test1;
+// import lib;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        test1.print();
+        // lib.libtest1.print();
+
+        lib.test2.libtest1.print();
+
+        
+    }
+}
